7d3f08166ee9c1c6f6f99ece12098abf0c5cf84c
themnchstr <themnchstr@gmail.com>
2022-12-03 01:35:12 +0500

    add DZ_5

---
+++ DZ_5/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход
+//трёхзначное число и на выходе показывает вторую цифру этого
+//числа.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int Number2nd = (number/10) % 10;
+
+Console.WriteLine($"{Number2nd}");
