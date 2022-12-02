ea04af26fd6125adf3b44ff282ae2892a59ba80d
themnchstr <themnchstr@gmail.com>
2022-12-03 01:11:16 +0500

    add DZ_3

---
+++ DZ_3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает
+//число и выдает, является ли число четным (делится ли оно на
+//два без остатка).
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+} 
