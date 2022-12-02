e13b5d57d961d9ffb175395abeb501b01d3556cf
themnchstr <themnchstr@gmail.com>
2022-12-03 01:19:37 +0500

    add DZ_4

---
+++ DZ_4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает
+//число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Enter N: ");
+int N = int.Parse(Console.ReadLine());
+
+int i = 1;
+while(i <= N)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write($"{i}");
+    }
+    i++;
+}
+Console.WriteLine();
