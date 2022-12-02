ef0d07a4bc633883fa1c670dc4abf5f751105bcb
themnchstr <themnchstr@gmail.com>
2022-12-03 01:02:18 +0500

    add DZ_2

---
+++ DZ_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход
+//три числа и выдает максимальное из этих чисел.
+
+Console.Write("Enter 1st number: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Enter 2nd number: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Enter 3th number: ");
+int number3 = int.Parse(Console.ReadLine());
+
+int max = number1;
+if(max < number2)
+{
+    max = number2;
+}
+if(max < number3)
+{
+    max = number3;
+}
+
+Console.WriteLine($"Max number = {max}");
