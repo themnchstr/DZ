2bf1039e710432da198ac2d6c7b17fd4c53e861f
themnchstr <themnchstr@gmail.com>
2022-12-03 00:52:57 +0500

    add DZ_1

---
+++ DZ_1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход принимает
+//два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Write("Enter number 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Enter number 2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("number 1 more number 2");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine("number 2 more number 1");
+}
+else
+{
+    Console.WriteLine("number 1 equally number 2");
+}
+
