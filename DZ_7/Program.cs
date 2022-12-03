e5db457b37663936c37d953984d251ab8e3ed03f
themnchstr <themnchstr@gmail.com>
2022-12-03 18:49:49 +0500

    add  DZ_7

---
+++ DZ_7/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход
+//цифру, обозначающую день недели, и проверяет, является ли
+//этот день выходным.
+
+Console.WriteLine("Enter day number : ");
+string enterDay = Console.ReadLine();
+int day = Convert.ToInt32(enterDay);
+string WeekDay = "Monday";
+
+if (day == 1 ) WeekDay = "No";
+if (day == 2 ) WeekDay = "No";
+if (day == 3 ) WeekDay = "No";
+if (day == 4 ) WeekDay = "No";
+if (day == 5 ) WeekDay = "No";
+if (day == 6 ) WeekDay = "Yes";
+if (day == 7 ) WeekDay = "Yes";
+
+Console.WriteLine(WeekDay);
