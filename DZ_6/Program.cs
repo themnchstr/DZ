b5c75bff77871d35dbb0e5629b3e46183f78937b
themnchstr <themnchstr@gmail.com>
2022-12-03 18:38:15 +0500

    add  DZ_6

---
+++ DZ_6/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая выводит третью
+//цифру заданного числа или сообщает,
+//что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int Num = number/10;
+int Num1 = number/100;
+
+while (Num > 999)
+{
+    Num = Num / 10;
+}
+if (Num1 == 0)
+{
+   Console.WriteLine("Третьего числа нет");
+}
+else if (Num > 100)
+{
+    Num = Num % 10;
+    Console.WriteLine($"Третье число {Num}");
+}
+else
+{
+    Num = number % 10;
+    Console.WriteLine($"Третье число {Num}");
+}
