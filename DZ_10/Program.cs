bc73221a8cabc7a1bc02c33a995a0ea2afaf82eb
themnchstr <themnchstr@gmail.com>
2022-12-06 12:14:33 +0500

    add  DZ_10

---
+++ DZ_10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на
+//вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("введите число:");
+int num = int.Parse(Console.ReadLine());
+int i = 1;
+while (i<=num)
+{  
+    Console.Write ($"{i*i*i} ,");
+    i++;
+}
