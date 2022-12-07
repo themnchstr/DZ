e72a994e074bb118b68beadb535b18f25bfac35a
themnchstr <themnchstr@gmail.com>
2022-12-07 20:41:35 +0500

    add  DZ_13

---
+++ DZ_13/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая задаёт массив
+//из 8 элементов и выводит их на экран.
+
+int lenght = 8;
+int[] array = new int[lenght];
+Console.WriteLine("Введите числа массива через 'Enter' ");
+
+void ReadArray(int[] array)
+{
+    int index = 0;
+    while (index < lenght)
+    {
+        array[index] = int.Parse(Console.ReadLine()!);
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int index = 0;
+    while (index < lenght)
+    {
+        Console.Write(array[index] + " ");
+        index++;
+    }
+}
+ReadArray(array);
+Console.Write("Массив в строку ");
+PrintArray(array);
