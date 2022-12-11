1bd9347d8ee69475609c1088690b1a7b4e5b5c22
themnchstr <themnchstr@gmail.com>
2022-12-11 16:32:57 +0500

    add  DZ_17

---
+++ DZ_17/Program.cs
@@ -0,0 +1,14 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите колличество чисел: ");
+int num = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < num; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Положительных чисел: {count}");
