735d03764cf041f2dbf8245a9b8b1f2ceafebe31
themnchstr <themnchstr@gmail.com>
2022-12-15 02:34:22 +0500

    add  DZ_24

---
+++ DZ_24/Program.cs
@@ -0,0 +1,69 @@
+﻿//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц
+
+Console.WriteLine("Введите числа для двух матриц");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int p = Convert.ToInt32(Console.ReadLine());
+int range = Convert.ToInt32(Console.ReadLine());
+
+int[,] Martrix = new int[m, n];
+CreateArray(Martrix);
+Console.WriteLine($"Первая матрица:");
+WriteArray(Martrix);
+
+int[,] Martrix2 = new int[n, p];
+CreateArray(Martrix2);
+Console.WriteLine($"Вторая матрица:");
+WriteArray(Martrix2);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(Martrix, Martrix2, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] Martrix, int[,] Martrix2, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < Martrix.GetLength(1); k++)
+      {
+        sum += Martrix[i,k] * Martrix2[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
