7696434c1d874c766111d1d8fc5c23f7ab7967a8
themnchstr <themnchstr@gmail.com>
2022-12-16 15:23:17 +0500

    add  DZ_27

---
+++ DZ_27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+//в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите положительное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+Console.WriteLine(Number(n, m));
+
+int Number(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{Number(n, m + 1)}, ");
+    return m;
+}
