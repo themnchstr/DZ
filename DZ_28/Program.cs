592276cd3cf32ff867b9fe006925a82b1b5dec30
themnchstr <themnchstr@gmail.com>
2022-12-16 20:38:02 +0500

    add  DZ_28

---
+++ DZ_28/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 66: Задайте значения M и N.
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+void Sum(int M, int N, int sum)
+{
+    if (M > N)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (M++);
+    Sum(M, N, sum);
+}
+
+Sum(M, N, sum);
